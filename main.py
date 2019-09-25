ee0ea3bfc1364f191e6b44113cc3a3b767b366ba
Saikat Mondol <saikatmandal8@gmail.com>
2019-09-26 01:21:12 +0600

    first build

---
+++ main.py
@@ -0,0 +1,14 @@
+"""Main fucntion for Magic 8 ball"""
+from mods.ball_code import MagicBall
+QUESTION = input("Ask your question : ")
+print(MagicBall.answers[MagicBall().ball_says()])
+while True:
+    CHOICE = input("Want to ask another question? (Y/n) : ")
+    if CHOICE == 'Y':
+        QUESTION = input("Ask your question : ")
+        print(MagicBall.answers[MagicBall().ball_says()])
+    elif CHOICE == 'n':
+        print("Exiting magic 8 ball...")
+        break
+    else:
+        print("Wrong choice, try again.")
---
+++ mods/ball_code.py
@@ -0,0 +1,21 @@
+"""module for magic ball"""
+from random import choice
+from time import sleep
+class MagicBall:
+    """class containing resources for magic ball"""
+    answers = {1: "As I see it, yes.", 2: "Ask again later.",
+               3: "Better not tell you now.", 4: "Cannot predict now.",
+               5: "Concentrate and ask again.", 6: "Don’t count on it.",
+               7: "It is certain.", 8: "It is decidedly so.",
+               9: "Most likely.", 10: "My reply is no.", 11: "My sources say no.",
+               12: "Outlook not so good.", 13: "Outlook good.",
+               14: "Reply hazy, try again.", 15: "Signs point to yes.",
+               16: "Very doubtful.", 17: " Without a doubt.", 18: "Yes.",
+               19: "Yes – definitely.", 20: "You may rely on it."}
+    def __init__(self):
+        pass
+    def ball_says(self):
+        """Lets see what the ball says"""
+        print("Thinking.....")
+        sleep(2)
+        return choice(list(self.answers.keys()))
